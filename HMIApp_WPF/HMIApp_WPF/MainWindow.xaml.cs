16b020ed9e2af3509196c5fa29a5f80c70136415
Maskul96 <maskulaniskamil96@gmail.com>
2024-11-13 20:49:50 +0100

    Dodanie wykresu ScottPlot

--- HMIApp_WPF/HMIApp_WPF/MainWindow.xaml.cs
+++ HMIApp_WPF/HMIApp_WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using ScottPlot.WPF;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,15 @@ namespace HMIApp_WPF
         public MainWindow()
         {
             InitializeComponent();
+
+            double[] xs = { 1, 2, 3, 4, 5 };
+            double[] ys = { 1, 4, 9, 16, 25 };
+
+            var scatter = wpfPlot.Plot.Add.Scatter(xs, ys);
+            //scatter.Color = System.Drawing.Color.Blue; // Ustaw kolor linii na niebieski
+            //wpfPlot.Plot.Grid(enable: true, color: System.Drawing.Color.LightGray, lineStyle: ScottPlot.LineStyle.Dash);
+
+            wpfPlot.Refresh();
         }
     }
 }
